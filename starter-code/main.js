c158b8a930af8f136ca6c3daf3ae8e6eaa72c328
Justin Kim <kimchee@Kimchees-Macbook-Pro-Touch.local>
2017-03-10 20:07:28 -0800

    fixed if else to else if

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,9 +6,10 @@ var cardFour = "king";
 if(cardOne == cardTwo){
 	alert("You found a match!");
 }
-if else(cardThree == cardFour){
+else if(cardThree == cardFour){
 	alert("You found a match!");
 }
 else{
 	alert("Sorry, try again.");
 }
+console.log("hi");
