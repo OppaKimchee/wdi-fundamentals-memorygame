8e3bfd2ec5f80887ec400f1d0bd83183826510a7
Justin Kim <kimchee@Kimchees-Macbook-Pro-Touch.local>
2017-03-15 03:58:35 -0700

    final version

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,11 +1,69 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//creating an array of cards
+var cards = ['queen', 'queen', 'king', 'king'];
+//cards that are played will be stored in here.
+var cardsInPlay = [];
 
-var createCards = function(x){
+//when did we create this....
+//instructions unclear, took me a while to realize createCards == createBoard...
+//creates the game board
+var createBoard = function(){
 
-	for(var i = 0; i < x; i++){
+	//loop to create the cards from the cards[]
+	for(var i = 0; i < cards.length; i++){
+
+		//create the space for the cards
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		//set the attribute for the div to data-card and gives it current card value
+		cardElement.setAttribute('data-card', cards[i]);
+		//listens for a mouse click event, then steps into isTwoCards function
+    	cardElement.addEventListener('click', isTwoCards);
+		document.getElementById('game-board').appendChild(cardElement);
+
+	}
+
+}//createBoard()
+
+//function for checking cards selected
+var isTwoCards = function(){
+
+	cardsInPlay.push(this.getAttribute('data-card'));
+	//check to assign king or queen picture to click event
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src = 'king.png' alt = 'King' />";
+	} 
+	else {
+		this.innerHTML = "<img src = 'queen.png' alt = 'Queen'/>";
+	}
+	//check when 2 cards are picked to step in isMatch to check for matches
+	if(cardsInPlay.length === 2){
+		isMatch(cardsInPlay);
+		//clear played cards
+		cardsInPlay = [];
+	}
+
+}//isTwoCards()
+
+//function to check for 
+var isMatch = function(){
+
+	//check cards in element 0 and 1 of cardsInPlay[]
+	if(cardsInPlay[0] == cardsInPlay[1] ){
+	alert("You found a match!");
+	}
+	else
+	alert("Sorry, try again.");
+	//reset the cards or "unflip" them.
+	this.innerHTML = '';
+
+}//isMatch()
+
+createBoard();
+
+/*
+var createCards = function(){
+
+	for(var i = 0; i < 4; i++){
 
 	var cardDiv = document.createElement('div');
 	cardDiv.innerHTML='<div class="card"></div>';
@@ -14,9 +72,8 @@ var createCards = function(x){
 	}
 
 }
-createCards(4);
+createCards();
 
-/*
 if(cardOne == cardTwo){
 	alert("You found a match!");
 }
